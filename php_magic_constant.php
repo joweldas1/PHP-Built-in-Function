84842bf9b6a7463beed3e5ca196f1237439eb5f6
joweldas1 <work.jowel@gmail.com>
2025-03-13 14:41:24 +0600

    PHP MAGIC CONSTANT

---
+++ php_magic_constant.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * PHP-তে Magic Constants হলো কিছু Predefined (পূর্বনির্ধারিত) Constant, যেগুলোর মান স্ক্রিপ্ট অনুযায়ী পরিবর্তন হয়।
+ * __LINE__
+ * __FILE__
+ * __DIR__
+ * __FUNCTION__
+ * __CLASS__
+ * __METHOD__
+ * __NAMESPACE__
+ * __TRAIT__
+ */
+
+
+ echo "This is line  number --->". __LINE__ ."<br>";
+ echo "This folder name --->". __FILE__ ."<br>";
+ echo "This   --->". __DIR__ ."<br>";
+ echo "This   --->". __FUNCTION__ ."<br>";
+ echo "This   --->". __CLASS__ ."<br>";
+ echo "This   --->". __METHOD__ ."<br>";
+ echo "This   --->". __NAMESPACE__ ."<br>";
+ echo "This   --->". __TRAIT__ ."<br>";
